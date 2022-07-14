6bcb9351c06dd95a89c3a549cc43be94dc34824b
bourbon <khanhnguyen297kn@gmail.com>
2022-07-14 18:26:29 +0700

    servo

--- servo.cpp
+++ servo.cpp
@@ -1,12 +1,47 @@
+//https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
+
+
+
 #include< Servo.h>
 Servo myservo;
 void setup() {
     Serial.begin(9600);
     myservo.attach(25);
 }
+int lookRight()
+{
+    myservo.write(50); 
+    delay(500);
+   // int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+   // return distance;
+}
 
+int lookLeft()
+{
+    myservo.write(170); 
+    delay(500);
+    //int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+   // return distance;
+    delay(100);
+}
+
+// int readPing() { 
+//   delay(70);
+//   int cm = sonar.ping_cm();
+//   if(cm==0)
+//   {
+//     cm = 250;
+//   }
+//   return cm;
+// }
 void loop() { 
-    myservo.write(90);
+   lookLeft();
+lookRight(); 
+
 }
 
 
@@ -18,4 +53,8 @@ void loop() {
 
 
 
-//https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
+
+
+
+
+
