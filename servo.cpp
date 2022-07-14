5fc3ea408049fdd00ecbdc65d8b2ff422aa96641
bourbon <khanhnguyen297kn@gmail.com>
2022-07-14 16:29:17 +0700

    servo

--- servo.cpp
+++ servo.cpp
@@ -1,2 +1,21 @@
 #include< Servo.h>
+Servo myservo;
+void setup() {
+    Serial.begin(9600);
+    myservo.attach(25);
+}
+
+void loop() { 
+    myservo.write(90);
+}
+
+
+
+
+
+
+
+
+
+
 //https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
